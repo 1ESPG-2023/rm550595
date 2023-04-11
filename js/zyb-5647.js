71107106ea466c7e35b0f30fdc8499e1edd4f510
joaogavadev <rm550595@fiap.com.br>
2023-04-11 11:50:36 -0300

    ultimas modificações das arrays

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,3 +1,18 @@
 
 const liElement = document.getElementsByTagName("li");
-console.log(liElement)
+
+
+
+// for (let index = 0; index < liElement.length; index++) {
+//     console.log(liElement[index].textContent)
+// }
+// console.log(liElement.length)
+
+//declaração de arrays
+let nm1 = [1,2,3,4,5];
+let nm2 = [6,7,8,9,10];
+let nm3 = [nm1,nm2];
+
+
+nm3.forEach((itemdaarray)=>
+console.log('array-3 sendo listado ${itemdaarray}');
